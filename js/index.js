d4b2de10cf328cb1621f59249186a2f702d1db17
Justin Hall <hall.justin.d@gmail.com>
2018-12-31 20:11:25 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -20,10 +20,10 @@ input.addEventListener('keydown', function(e){
 
 let divs = document.querySelectorAll('div');
 
-function bubble(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
 
-for (let i=0; i <divs.length; i++) {
-  divs[i].addEventListener('click', bubble);
+for (let i=0; i < divs.length; i++) {
+  divs[i].addEventListener('click', capture, true);
 }
